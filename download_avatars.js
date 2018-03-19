db2b15bb2a6bef268d2dcc3a6aaaeedf2349e9c2
Craig Rice <craigricetong@gmail.com>
2018-03-19 23:20:45 +0000

    added authentification

--- download_avatars.js
+++ download_avatars.js
@@ -1,8 +1,20 @@
 var request = require('request');
+var auth = require('./secrets');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': auth
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
 }
+
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -12,3 +24,7 @@ getRepoContributors("nodejs", "node", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
+
+
+
+
