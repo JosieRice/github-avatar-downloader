3a95516596d9a1cf3ace3982ca59e6d234c37755
Craig Rice <craigricetong@gmail.com>
2018-03-19 22:58:18 +0000

    added calling function with hard coded values

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,14 @@
 var request = require('request');
 
+function getRepoContributors(repoOwner, repoName, cb) {
+  // ...
+}
+
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+
+
+getRepoContributors("nodejs", "node", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
